4ec153439482e996d5952babd40a2cb3667b9449
dodi1772 <zsigmond.dorian01@gmail.com>
2025-01-14 10:32:08 +0100

    Blabla

--- OOP2/Program.cs
+++ OOP2/Program.cs
@@ -4,7 +4,11 @@
 	{
 		static void Main(string[] args)
 		{
-
-		}
+			Pont pont = new Pont();
+            Console.WriteLine(pont);
+			Pont masodikpont = new Pont(20,30);
+            Console.WriteLine($"X: {masodikpont.x} Y: {masodikpont.y}");
+            Console.WriteLine(pont.tavolsagmeres(masodikpont));
+        }
 	}
 }
--- OOP2/pont.cs
+++ OOP2/pont.cs
@@ -6,17 +6,69 @@ using System.Threading.Tasks;
 
 namespace OOP2
 {
-	internal class pont
+	internal class Pont
 	{
 		private int X;
 		private int Y;
 		private string Nev;
-		public pont(int X, int Y, string Nev)
+		public int x
 		{
-			this.X = X;
-			this.Y = Y;
-			this.Nev = Nev;
+			get { return X; }
+			set { X = value; }
 		}
-
+		public int y
+		{
+			get { return Y; }
+			set { Y = value; }
+		}
+		public string nev
+		{
+			get { return Nev; }
+			set { Nev = value; }
+		}
+		public Pont(int x, int y, string nev = "pont")
+		{
+			X = x;
+			Y = y;
+			Nev = nev;
+		}
+		public Pont()
+		{
+			Nev = "pont1";
+			X = 1;
+			Y = 3;
+		}
+		public Pont(int interv)
+		{
+			Random random = new Random();
+			int randomNumberX = random.Next(-interv, interv);
+			X = randomNumberX;
+			int randomNumberY = random.Next(-interv, interv);
+			Y = randomNumberY;
+            Console.WriteLine($"X: {X} Y:{Y}");
+        }
+		public double tavolsagmeres(Pont MasikPont)
+		{
+			int kulonbsegX=x-MasikPont.x;
+			int kulonbsegY=y-MasikPont.y;
+			return Math.Sqrt(Math.Pow(kulonbsegX, 2)+Math.Pow(kulonbsegY,2));
+		}
+		public void PontCsere()
+		{
+			int temp = x;
+			x = y;
+			y = temp;
+            Console.WriteLine("A pontcsere sikeres.");
+        }
+		public void Nagyobbitas(int szorzo)
+		{
+			x=x*szorzo;
+			y=y*szorzo;
+            Console.WriteLine("A nagyobbítás sikeres");
+        }
+		public bool IsOr()
+		{
+			return x == 0 && y == 0;
+		}
 	}
 }
