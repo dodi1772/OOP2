f8632ed8b4d98b9733342e04268191ea9a23d795
Diák <Dik@PETRIK>
2025-01-14 09:32:42 +0100

    Add project files.

---
+++ OOP2/pont.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP2
+{
+	internal class pont
+	{
+		private int X;
+		private int Y;
+		private string Nev;
+		public pont(int X, int Y, string Nev)
+		{
+			this.X = X;
+			this.Y = Y;
+			this.Nev = Nev;
+		}
+
+	}
+}
